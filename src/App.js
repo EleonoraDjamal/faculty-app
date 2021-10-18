17fedce400b6cc3aac59a1f3d42c8a555cd065db
reyr <reymonr@universitasklabat.onmicrosoft.com>
2021-10-18 14:52:12 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Welcome name="Septiano" prodi="TI" />
+      <Welcome name="Fitriani" prodi={2} gender="Female" />
+
+      <Hello name="Jordan" prodi="SI" year={2021} />
     </div>
   );
 }
